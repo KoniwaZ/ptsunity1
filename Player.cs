bbdcb668fb480e5c1e897eedb4e80ac8ad8e6272
KoniwaZ <124517346+KoniwaZ@users.noreply.github.com>
2023-02-15 23:55:11 +0700

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -5,49 +5,116 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    Vector3 touchPosition;
-    Vector3 direction;
-
     [SerializeField] int speed;
+    [SerializeField] int jump;
 
-    [SerializeField] Text ScoreText;
+    [SerializeField] GroundChecker groundCheck;
+
+    [SerializeField] Text scoreText;
+    [SerializeField] Text lifeText;
+    [SerializeField] Text notifText;
+    [SerializeField] Text lastText;
+
+    [SerializeField] Button restartButton;
+    [SerializeField] Button mainmenuButton;
+    [SerializeField] Button nextstageButton;
+
+    [SerializeField] GameObject treasure;
 
     Rigidbody2D rb;
 
-    private int totalScore;
+    bool isWin;
+
+    private int score;
+    private int life;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        totalScore = 0;
-        ScoreText.text = "Score : 0";
+        isWin = false;
+        score = 0;
+        life = 3;
+        scoreText.text = "Score : " + score;
+        lifeText.text = life +" : Life";
+        notifText.gameObject.SetActive(false);
+        lastText.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
+        mainmenuButton.gameObject.SetActive(false);
+        nextstageButton.gameObject.SetActive(false);
+
     }
+
     void Update()
     {
         var x = Input.GetAxis("Horizontal");
-        var y = Input.GetAxis("Vertical");
+        
 
-        rb.velocity = new Vector2(x, y) * speed;
-
-        /*if (Input.touchCount > 0)
+        if(groundCheck.isGround)
         {
-            if(Input.touchCount < 2)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                Touch touch = Input.GetTouch(0);
-                touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-                touchPosition.z = 0;
-                direction = (touchPosition - transform.position);
-                rb.velocity = new Vector2(direction.x, direction.y) * speed;
-                if(touch.phase ==  TouchPhase.Ended)
+                rb.velocity = new Vector2(rb.transform.position.x,jump);
+                groundCheck.isGround = false;
+            }
+            
+        }
+        rb.AddForce(new Vector2(x, 0));
+        rb.velocity = new Vector2(x* speed * Time.deltaTime, rb.velocity.y) ;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("PickUps"))
+        {
+            if (!isWin)
+            {
+                score++;
+                scoreText.text = "Score : " + score;
+                Destroy(collision.gameObject);
+            }
+        }
+        if(collision.gameObject.CompareTag("Finish"))
+        {
+            if(!isWin)
+            {
+                isWin = true;
+                Succes();
+            }
+        }
+        if(collision.gameObject.CompareTag("Traps"))
+        {
+            if(!isWin)
+            {
+                life--;
+                lifeText.text = life + " : Life";
+                //transform.position = new Vector3(-8, -1,1 );
+                if(life <= 0)
                 {
-                    rb.velocity = Vector2.zero;
+                    life = 0;
+                    Fail();
                 }
             }
-        }*/
+        }
     }
- 
-    public void UpdateScore(int score)
+
+    private void Succes()
     {
-        totalScore += score;
-        ScoreText.text = "Score : " + totalScore;
+        lastText.text = "Score : " + score;
+        notifText.text = "Stage 1 Completed!!!";
+        notifText.gameObject.SetActive(true);
+        lastText.gameObject.SetActive(true);
+        mainmenuButton.gameObject.SetActive(true);
+        nextstageButton.gameObject.SetActive(true);
+        treasure.gameObject.SetActive(false);
+    }
+    
+    private void Fail()
+    {
+        lastText.text = "Score : " + score;
+        notifText.text = "Game Over!!!";
+        notifText.gameObject.SetActive(true);
+        lastText.gameObject.SetActive(true);
+        mainmenuButton.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        gameObject.SetActive(false);
     }
 }
